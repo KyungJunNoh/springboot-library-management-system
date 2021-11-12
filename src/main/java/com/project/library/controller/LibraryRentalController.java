67f4a355e8ba8e1ee4a5dec5ef6b00fd6da2ef65
노경준 <developerjun0615@gmail.com>
2021-11-12 14:01:53 +0900

    Update rental 리턴 값 부여

--- src/main/java/com/project/library/controller/LibraryRentalController.java
+++ src/main/java/com/project/library/controller/LibraryRentalController.java
@@ -1,5 +1,7 @@
 package com.project.library.controller;
 
+import com.project.library.Response.CommonResult;
+import com.project.library.Response.ResponseService;
 import com.project.library.dto.RentalBookDto;
 import com.project.library.service.LibraryRentalService;
 import io.swagger.annotations.ApiImplicitParam;
@@ -17,14 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryRentalController {
 
     private final LibraryRentalService libraryRentalService;
+    private final ResponseService responseService;
 
     @PostMapping("/rental")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
     })
     @ApiOperation(value = "도서 대출")
-    public void rental(@RequestBody RentalBookDto rentalBookDto){
+    public CommonResult rental(@RequestBody RentalBookDto rentalBookDto) {
         libraryRentalService.rental(rentalBookDto);
+        return responseService.commonResult();
     }
-
 }
