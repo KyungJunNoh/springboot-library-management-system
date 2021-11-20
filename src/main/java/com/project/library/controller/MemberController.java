86750c754a70de0db2ef29e3d16761d20c7944b3
노경준 <developerjun0615@gmail.com>
2021-11-20 17:49:23 +0900

    ADD MemberController 비밀번호 변경 기능 구현

--- src/main/java/com/project/library/controller/MemberController.java
+++ src/main/java/com/project/library/controller/MemberController.java
@@ -1,14 +1,17 @@
 package com.project.library.controller;
 
+import com.project.library.dto.UpdatePasswordDto;
 import com.project.library.response.CommonResult;
 import com.project.library.response.ResponseService;
 import com.project.library.dto.MemberDto;
 import com.project.library.dto.SigninDto;
 import com.project.library.service.MemberService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.sql.Update;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -33,6 +36,16 @@ public class MemberController {
     @ApiOperation(value = "로그인")
     public Map<String, String> signin(@RequestBody SigninDto signinDto){
         return memberService.signin(signinDto);
+    }
+
+    @PutMapping("/update")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+    })
+    @ApiOperation(value = "비밀번호 변경")
+    public CommonResult update(@RequestBody UpdatePasswordDto updatePassword){
+        memberService.update(updatePassword);
+        return responseService.commonResult();
     }
 
     @DeleteMapping("/logout")
