55f2551b5cbf3c3fde326976dfe193113a767e24
노경준 <developerjun0615@gmail.com>
2021-11-20 17:49:57 +0900

    Update MemberService 비밀번호 변경 기능 구현

--- src/main/java/com/project/library/service/MemberService.java
+++ src/main/java/com/project/library/service/MemberService.java
@@ -2,11 +2,13 @@ package com.project.library.service;
 
 import com.project.library.dto.MemberDto;
 import com.project.library.dto.SigninDto;
+import com.project.library.dto.UpdatePasswordDto;
 
 import java.util.Map;
 
 public interface MemberService {
     void signUp(MemberDto memberDto);
     Map<String,String> signin(SigninDto signinDto);
+    void update(UpdatePasswordDto updatePasswordDto);
     void logout();
 }
