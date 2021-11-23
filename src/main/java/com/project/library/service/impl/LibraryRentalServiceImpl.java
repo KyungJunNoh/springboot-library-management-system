25a566d48c89756c7a628f60b7e7c957384fdaa1
노경준 <developerjun0615@gmail.com>
2021-11-23 16:20:23 +0900

    Update LibraryRentalServiceImpl QueryDSL을 사용하여 책이름으로 대출 정보를 조회해오는 기능 구현

--- src/main/java/com/project/library/service/impl/LibraryRentalServiceImpl.java
+++ src/main/java/com/project/library/service/impl/LibraryRentalServiceImpl.java
@@ -1,13 +1,13 @@
 package com.project.library.service.impl;
 
 import com.project.library.dto.ExtensionBook;
-import com.project.library.dto.FindRentalBookDto;
 import com.project.library.dto.RentalBookDto;
 import com.project.library.dto.ReturnBookDto;
 import com.project.library.model.Book;
 import com.project.library.model.Member;
 import com.project.library.model.Rental;
 import com.project.library.repository.BookRepository;
+import com.project.library.repository.RentalCustomRepository;
 import com.project.library.repository.RentalRepository;
 import com.project.library.service.LibraryRentalService;
 import com.project.library.util.CurrentUserUtil;
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +25,7 @@ public class LibraryRentalServiceImpl implements LibraryRentalService {
 
     private final BookRepository bookRepository;
     private final RentalRepository rentalRepository;
+    private final RentalCustomRepository rentalCustomRepository;
     private final CurrentUserUtil currentUserUtil;
 
     @Override
@@ -70,5 +70,14 @@ public class LibraryRentalServiceImpl implements LibraryRentalService {
         Map<Long,Rental> map = new HashMap<>();
         map.put(idx,findBook);
         return map;
+    }
+
+    @Override
+    public List<Rental> findRentalBookByBookName(String book) {
+        List<Rental> rental = rentalCustomRepository.findRentalBookByBookName(book);
+
+        if (rental.isEmpty()) throw new IllegalArgumentException(book + "에 해당하는 책을 찾을 수 없습니다.");
+
+        return rental;
     }
 }
