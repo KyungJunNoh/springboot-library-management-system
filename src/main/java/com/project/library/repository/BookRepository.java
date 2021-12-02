70b3980707b7360d3351817629975761ddca07de
노경준 <developerjun0615@gmail.com>
2021-12-03 01:27:56 +0900

    Update BookRepository Optional 값 씌워주기

--- src/main/java/com/project/library/repository/BookRepository.java
+++ src/main/java/com/project/library/repository/BookRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
 
-    List<Book> findBookByTitle(String title);
+    Optional<List<Book>> findBookByTitle(String title);
 }
